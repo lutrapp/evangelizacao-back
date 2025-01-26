2516923ea1d076f1984c5375bc569cdecf30d324
lutrapp <lutrapp@gmail.com>
2025-01-25 23:58:25 -0300

    fisrst commit empty springboot project by initializ

---
+++ src/test/java/com/evangelizacao_back/assistance/AssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evangelizacao_back.assistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
